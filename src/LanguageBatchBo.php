946116c1394357889fc5e0ceb2a8920d39dcba8b
daniel archer <contato@danielcramer.com.br>
2017-11-25 20:42:06 -0200

    created isolated method to handle applet file

--- src/LanguageBatchBo.php
+++ src/LanguageBatchBo.php
@@ -113,31 +113,36 @@ class LanguageBatchBo
 
 		echo "\nGetting applet language XMLs..\n";
 
-		foreach ($applets as $appletDirectory => $appletLanguageId) {
-			echo " Getting > $appletLanguageId ($appletDirectory) language xmls..\n";
-			$languages = self::getAppletLanguages($appletLanguageId);
-			if (empty($languages)) {
-				throw new \Exception('There is no available languages for the ' . $appletLanguageId . ' applet.');
-			}
-			else {
-				echo ' - Available languages: ' . implode(', ', $languages) . "\n";
-			}
-			$path = Config::get('system.paths.root') . '/cache/flash';
-			foreach ($languages as $language) {
-				$xmlContent = self::getAppletLanguageFile($appletLanguageId, $language);
-				$xmlFile    = $path . '/lang_' . $language . '.xml';
-				if (strlen($xmlContent) == file_put_contents($xmlFile, $xmlContent)) {
-					echo " OK saving $xmlFile was successful.\n";
-				}
-				else {
-					throw new \Exception('Unable to save applet: (' . $appletLanguageId . ') language: (' . $language
-						. ') xml (' . $xmlFile . ')!');
-				}
-			}
-			echo " < $appletLanguageId ($appletDirectory) language xml cached.\n";
+		foreach ($applets as $appletLanguageId) {
+			self::generateAppletXmlFile($appletLanguageId);
 		}
 
 		echo "\nApplet language XMLs generated.\n";
+	}
+
+	protected static function generateAppletXmlFile($appletLanguageId)
+	{
+		echo " Getting > $appletLanguageId language xmls..\n";
+		$languages = self::getAppletLanguages($appletLanguageId);
+		if (empty($languages)) {
+			throw new \Exception('There is no available languages for the ' . $appletLanguageId . ' applet.');
+		}
+		else {
+			echo ' - Available languages: ' . implode(', ', $languages) . "\n";
+		}
+		$path = Config::get('system.paths.root') . '/cache/flash';
+		foreach ($languages as $language) {
+			$xmlContent = self::getAppletLanguageFile($appletLanguageId, $language);
+			$xmlFile    = $path . '/lang_' . $language . '.xml';
+			if (strlen($xmlContent) == file_put_contents($xmlFile, $xmlContent)) {
+				echo " OK saving $xmlFile was successful.\n";
+			}
+			else {
+				throw new \Exception('Unable to save applet: (' . $appletLanguageId . ') language: (' . $language
+					. ') xml (' . $xmlFile . ')!');
+			}
+		}
+		echo " < $appletLanguageId language xml cached.\n";
 	}
 
 	/**
