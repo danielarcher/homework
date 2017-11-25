137a892af38107edf3e38dc62c0fa8e1a2c46f98
daniel archer <contato@danielcramer.com.br>
2017-11-25 21:34:14 -0200

    new class to verify errors

--- src/LanguageBatchBo.php
+++ src/LanguageBatchBo.php
@@ -217,20 +217,6 @@ class LanguageBatchBo
 	 */
 	protected static function checkForApiErrorResult($result)
 	{
-		// Error during the api call.
-		if ($result === false || !isset($result['status'])) {
-			throw new \Exception('Error during the api call');
-		}
-		// Wrong response.
-		if ($result['status'] != 'OK') {
-			throw new \Exception('Wrong response: '
-				. (!empty($result['error_type']) ? 'Type(' . $result['error_type'] . ') ' : '')
-				. (!empty($result['error_code']) ? 'Code(' . $result['error_code'] . ') ' : '')
-				. ((string)$result['data']));
-		}
-		// Wrong content.
-		if ($result['data'] === false) {
-			throw new \Exception('Wrong content!');
-		}
+		ApiCallErrorVerifier::checkError($result);
 	}
 }
