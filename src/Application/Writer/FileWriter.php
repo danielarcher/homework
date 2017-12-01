c96a384fede753cad8469d63ccfbeb9922b5dd82
Daniel Archer <daniel.archer@kinghost.com.br>
2017-12-01 14:41:23 -0200

    simplified file writer

--- src/Application/Writer/FileWriter.php
+++ src/Application/Writer/FileWriter.php
@@ -6,23 +6,18 @@ class FileWriter implements WriterInterface
 {
 	public function write($destination, $content)
 	{
-		$this->prepareDirectory(dirname($destination));
+		if (!is_dir(dirname($dirname)) {
+			mkdir(dirname($dirname, 0755, true);
+		}
+
+		if (false === is_writable(dirname($dirname)) {
+			throw new \LogicException('Unable to write in: ' . $destination . '!');
+		}
 
 		if (strlen($content) !== file_put_contents($destination, $content)) {
 			throw new \LogicException('Unable to save file: ' . $destination . '!');
 		}
 
 		return true;
-	}
-
-	public function prepareDirectory($dirname)
-	{
-		if (!is_dir($dirname)) {
-			mkdir($dirname, 0755, true);
-		}
-
-		if (false === is_writable($dirname)) {
-			throw new \LogicException('Unable to write in: ' . $destination . '!');
-		}
 	}
 }
